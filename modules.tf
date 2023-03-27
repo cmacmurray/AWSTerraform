b9374223a6432d5c7bab285efaaf7f0112194076
Christopher MacMurray <chris.macmurray@gmail.com>
2023-03-27 19:40:44 -0400

    moved zone file to module

--- modules.tf
+++ modules.tf
@@ -1,4 +1,5 @@
-module "route53_complete" {
-  source  = "terraform-aws-modules/route53/aws//examples/complete"
+module "route53" {
+  source  = "terraform-aws-modules/route53/aws"
   version = "2.10.2"
+  route53_zone_name = "omnirextech.com"
 }
