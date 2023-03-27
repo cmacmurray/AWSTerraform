3931fb1b12839b997434a1518f4b8c4b0f86b749
Christopher MacMurray <chris.macmurray@gmail.com>
2023-03-27 19:50:51 -0400

    fixed up module mess

--- modules.tf
+++ modules.tf
@@ -1,5 +1,21 @@
-module "route53" {
-  source  = "terraform-aws-modules/route53/aws"
-  version = "2.10.2"
-  route53_zone_name = "omnirextech.com"
+module "zones" {
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "~> 2.0"
+
+  zones = {
+    "terraform-omnirex.com" = {
+      comment = "Omnirex main public zone file."
+      tags = {
+        env = "production"
+      }
+    }
+
+    "omnirex.com" = {
+      comment = "omnirex.com"
+    }
+  }
+
+  tags = {
+    ManagedBy = "Terraform"
+  }
 }
